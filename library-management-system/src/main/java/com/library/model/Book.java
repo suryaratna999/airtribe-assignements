f9fd9f182f74b42e50707d33cb49863216883732
“suryaratna999” <suryaratna999@gmail.com>
2025-09-30 00:13:10 +0530

    Added Model and Service classes

---
+++ library-management-system/src/main/java/com/library/model/Book.java
@@ -0,0 +1,21 @@
+package com.library;
+
+public class Book {
+    private final String id;
+    private String title;
+    private String author;
+
+    public Book(String id, String title, String author) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+    }
+
+    // Getters and setters
+    public String getId() { return id; }
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+
+    public void setTitle(String title) { this.title = title; }
+    public void setAuthor(String author) { this.author = author; }
+}
---
+++ library-management-system/src/main/java/com/library/service/BookInventory.java
@@ -0,0 +1,6 @@
+package com.library.service;
+
+public class InventoryManagement {
+
+    
+}
